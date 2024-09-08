81dceea5577ef8eb18024e3adf5475e7d0dc99c7
Shubh A Chudasama <shubh.cs@proton.me>
2024-09-08 21:29:37 +0530

    Set deploymentBranch to gh-pages in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -33,6 +33,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "gdsccvr", // Usually your GitHub org/user name.
   projectName: "gdsccvr.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
